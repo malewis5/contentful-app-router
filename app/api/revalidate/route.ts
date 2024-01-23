4d0eb3c43c75b191d90a5529363133dbe4c74fae
malewis5 <matthew.a.lewis7@gmail.com>
2024-01-23 11:06:46 -0500

    init

---
+++ app/api/revalidate/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server';
+import { revalidateTag } from 'next/cache';
+
+export async function POST(request: Request) {
+  const requestHeaders = new Headers(request.headers);
+  const secret = requestHeaders.get('x-vercel-revalidation-key');
+
+  if (secret !== process.env.CONTENTFUL_REVALIDATION_SECRET) {
+    return NextResponse.json({ message: 'Invalid secret' }, { status: 401 });
+  }
+
+  const body = await request.json();
+  const tag = body.slug['en-US'];
+
+  if (!tag) {
+    return NextResponse.json({ message: 'No tag provided' }, { status: 400 });
+  }
+
+  revalidateTag(tag);
+
+  return NextResponse.json({ revalidated: true, now: Date.now() });
+}
