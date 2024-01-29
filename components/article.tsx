418395c9f236d455377f06289165ae51ee9455a2
malewis5 <matthew.a.lewis7@gmail.com>
2024-01-29 12:57:28 -0500

    rework

--- components/article.tsx
+++ components/article.tsx
@@ -2,14 +2,16 @@
 
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import Image from 'next/image';
-import { useContentfulLiveUpdates } from '@contentful/live-preview/react';
-import { ContentfulLivePreview } from '@contentful/live-preview';
+import {
+  useContentfulInspectorMode,
+  useContentfulLiveUpdates,
+} from '@contentful/live-preview/react';
 import { ContentfulBlogProps } from '@/app/blogs/[slug]/page';
 
 export const Blog = ({ blog }: { blog: ContentfulBlogProps }) => {
-  const updatedBlog = useContentfulLiveUpdates(blog);
+  const data = useContentfulLiveUpdates(blog);
+  const inspectorProps = useContentfulInspectorMode({ entryId: data.sys.id });
 
-  console.log(ContentfulLivePreview);
   return (
     <main className='bg-white dark:bg-gray-900'>
       <section className='relative h-[500px] md:h-[600px] lg:h-[700px] xl:h-[800px] 2xl:h-[900px]'>
@@ -21,53 +23,48 @@ export const Blog = ({ blog }: { blog: ContentfulBlogProps }) => {
             aspectRatio: '1600/900',
             objectFit: 'cover',
           }}
-          src={updatedBlog?.heroImage?.url ?? 'https://placehold.co/650x365'}
+          src={data?.heroImage?.url ?? 'https://placehold.co/650x365'}
           width='1600'
-          {...ContentfulLivePreview.getProps({
-            entryId: blog.sys.id,
+          {...inspectorProps({
             fieldId: 'articleImage',
           })}
         />
         <div className='absolute inset-0 bg-black bg-opacity-50' />
         <div className='relative z-10 max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 h-full flex flex-col justify-center items-start'>
           <h1
-            {...ContentfulLivePreview.getProps({
-              entryId: blog.sys.id,
+            {...inspectorProps({
               fieldId: 'title',
             })}
             className='text-4xl font-extrabold tracking-tight text-white sm:text-5xl md:text-6xl'
           >
-            {updatedBlog.title}
+            {data.title}
           </h1>
           <p
-            {...ContentfulLivePreview.getProps({
-              entryId: blog.sys.id,
+            {...inspectorProps({
               fieldId: 'summary',
             })}
             className='mt-4 text-lg text-gray-300'
           >
-            {updatedBlog.summary}
+            {data.summary}
           </p>
           <p
             className='mt-4 text-md text-gray-400'
-            {...ContentfulLivePreview.getProps({
-              entryId: blog.sys.id,
+            {...inspectorProps({
               fieldId: 'author',
             })}
           >
-            By {updatedBlog.author}
+            By {data.author}
           </p>
         </div>
       </section>
       <section className='max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 py-12'>
         <article
           className='mx-auto'
-          {...ContentfulLivePreview.getProps({
-            entryId: blog.sys.id,
+          {...inspectorProps({
             fieldId: 'details',
           })}
         >
-          {documentToReactComponents(updatedBlog.details.json)}
+          {documentToReactComponents(data.details.json)}
         </article>
       </section>
     </main>
--- components/contentful-preview-provider.tsx
+++ components/contentful-preview-provider.tsx
@@ -1,14 +1,15 @@
 'use client';
 
+import { ContentfulLivePreviewInitConfig } from '@contentful/live-preview';
 import { ContentfulLivePreviewProvider } from '@contentful/live-preview/react';
+import { PropsWithChildren } from 'react';
 
 export function ContentfulPreviewProvider({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+  ...props
+}: PropsWithChildren<ContentfulLivePreviewInitConfig>) {
   return (
-    <ContentfulLivePreviewProvider locale='en-US' debugMode={true}>
+    <ContentfulLivePreviewProvider locale={props.locale}>
       {children}
     </ContentfulLivePreviewProvider>
   );
