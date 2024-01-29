20289a1e734460d491b98ec897a00e0c5b1fa61d
malewis5 <matthew.a.lewis7@gmail.com>
2024-01-29 11:38:38 -0500

    move provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import '@contentful/live-preview/style.css';
-import { ContentfulPreviewProvider } from '@/components/contentful-preview-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
-        <ContentfulPreviewProvider>{children}</ContentfulPreviewProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
